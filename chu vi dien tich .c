220e0867dd4e28d3c35e3ef18ea601050fed57b1
Trương Anh Đức <truongduc270906@gmail.com>
2025-04-03 10:50:26 +0700

    Update chu vi dien tich .c

--- chu vi dien tich .c
+++ chu vi dien tich .c
@@ -7,8 +7,8 @@ int main() {
     
     int chu_vi = 2 * (chieu_dai + chieu_rong);
     long long dien_tich = (long long)chieu_dai * chieu_rong;
-    printf("Chu vi HCN la: %d\n", chu_vi);
-    printf("Dien tich HCN la: %lld\n", dien_tich);
+    printf("Chu vi HCN la : %d\n", chu_vi);
+    printf("Dien tich HCN la : %lld\n", dien_tich);
     
     return 0;
 }
